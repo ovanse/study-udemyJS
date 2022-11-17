16565f6d2500a02f55e8fe30c31ae9eb37e4ef23
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-11-17 14:54:33 +1000

    практика 2

--- js/app.js
+++ js/app.js
@@ -8,12 +8,27 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов', ''),
-  b = prompt('На скольок оцените его?', ''),
-  c = prompt('Один из последних просмотренных фильмов', ''),
-  d = prompt('На скольок оцените его?', '');
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов', ''),
+    b = prompt('На скольок оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+
+if (personalMovieDB.coun < 10) {
+  console.log('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+  console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
