3cc2c6e11fe04cc9a6bb45c32824cac751ca3b53
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-11-18 16:39:44 +1000

    objects

--- js/tmp.js
+++ js/tmp.js
@@ -1,23 +1,37 @@
-function first() {
-  setTimeout(() => {
-    console.log(1);
-  },500);
+// const obj = new Object();
+
+const options = {
+  name: 'test',
+  width: 1024,
+  height: 1024,
+  colors: {
+    border: 'black',
+    bg: 'red',
+  },
+  makeTest: function() {
+    console.log("Test");
+  }
+};
+
+options.makeTest();
+
+console.log(Object.keys(options).length);
+
+const {border, bg} = options.colors;
+console.log(border);
+
+
+// console.log(options.name);
+
+// delete options.name;
+// console.log(options);
+
+for (let key in options) {
+  if (typeof(options[key]) === 'object') {
+    for (let i in options[key]) {
+      console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+    }
+  } else {
+    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+  }
 }
-
-function second() {
-  console.log(2);
-}
-
-first();
-second();
-
-function learnJS(lang, callback) {
-  console.log(`I study ${lang}.`);
-  callback();
-}
-
-function done() {
-  console.log('I passed this lesson.');
-}
-
-learnJS('JavaScript', done);
