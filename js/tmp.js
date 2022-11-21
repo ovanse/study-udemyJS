d39a9167c2e2326d3f3b17effb883d91307bd41e
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-11-21 11:28:39 +1000

    arrays full

--- js/tmp.js
+++ js/tmp.js
@@ -1,4 +1,11 @@
-const arr = [1, 2, 3, 6, 8];
+const arr = [2,13,26,8,10];
+
+// Способ правильно отсортировать массив чисел
+arr.sort(compareNums);
+console.log(arr);
+function compareNums(a,b) {
+  return a - b;
+}
 
 // arr.pop();
 // arr.push(10);
@@ -9,6 +16,16 @@ const arr = [1, 2, 3, 6, 8];
 //   console.log(arr[i]);
 // }
 
-for (let value of arr) {
-  console.log(value);
-}
+// for (let value of arr) {
+//   console.log(value);
+// }
+
+// arr.forEach(function(item, i, arr) {
+//   console.log(`${i}: ${item} in array ${arr}`);
+// })
+
+// const str = 'tea, coffee, beef, cheese';
+// const products = str.split(', ');
+// console.log(products);
+// products.sort();
+// console.log(products.join('; '));
