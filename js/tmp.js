21a1ee55b002968962c596aab3b71a601f3b21ef
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-11-21 16:23:04 +1000

    dynamic types

--- js/tmp.js
+++ js/tmp.js
@@ -1,27 +1,43 @@
-/* let str = 'some';
-let strObj = new String(str);
+// To String
 
-console.log(typeof(str));
-console.log(typeof(strObj));
+// 1)
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
 
-console.dir([1,2,3]); */
+// 2)
+console.log(typeof(5 + ''));
 
-const soldier = {
-  health: 400,
-  armor: 100,
-  sayHello: function() {
-    console.log('Hello');
-  }
-};
+const num = 5;
 
-const john = Object.create(soldier);
+console.log('https://vk.com/catalog/' + num);
 
-// const john = {
-//   health: 100,
-// };
+const fontSize = 26 + 'px';
 
-// john.__proto__ = soldier;
-// Object.setPrototypeOf(john, soldier);
+// To Number
 
-console.log(john.armor);
-john.sayHello();
+// 1)
+console.log(typeof(Number('4')));
+
+// 2)
+console.log(typeof(+'4'));
+let answ = +prompt('Hello','');
+
+// 3)
+console.log(typeof(parseInt('15px', 10)));
+
+// To Boolean
+
+// 0, '', null, undefined, NaN = false
+// other = true
+
+let switcher = null;
+
+if (switcher) {
+  console.log('Working...');
+}
+
+switcher = 1;
+
+if (switcher) {
+  console.log('Working...');
+}
