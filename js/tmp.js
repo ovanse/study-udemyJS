879bb80788256573863022a00709e8d2621a8ac0
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-11-21 10:52:12 +1000

    arrays

--- js/tmp.js
+++ js/tmp.js
@@ -1,37 +1,14 @@
-// const obj = new Object();
+const arr = [1, 2, 3, 6, 8];
 
-const options = {
-  name: 'test',
-  width: 1024,
-  height: 1024,
-  colors: {
-    border: 'black',
-    bg: 'red',
-  },
-  makeTest: function() {
-    console.log("Test");
-  }
-};
+// arr.pop();
+// arr.push(10);
 
-options.makeTest();
+// console.log(arr);
 
-console.log(Object.keys(options).length);
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
 
-const {border, bg} = options.colors;
-console.log(border);
-
-
-// console.log(options.name);
-
-// delete options.name;
-// console.log(options);
-
-for (let key in options) {
-  if (typeof(options[key]) === 'object') {
-    for (let i in options[key]) {
-      console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-    }
-  } else {
-    console.log(`Свойство ${key} имеет значение ${options[key]}`);
-  }
+for (let value of arr) {
+  console.log(value);
 }
