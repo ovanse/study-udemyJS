334604873846725773cadaaa12bca2d7e19ebb49
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-11-23 12:54:17 +1000

    closure

--- js/tmp.js
+++ js/tmp.js
@@ -1,4 +1,4 @@
-let number = 5; debugger
+/* let number = 5; debugger
 
 function logNumber() {
   console.log(number); debugger
@@ -8,3 +8,27 @@ function logNumber() {
 number = 6;
 
 logNumber(); debugger
+
+number = 8;
+
+logNumber(); debugger */
+
+function createCounter() {
+  let counter = 0;
+
+  const  myFunction = function() {
+    counter = counter + 1;
+    return counter;
+  }
+
+  return myFunction;
+}
+
+const increment = createCounter();
+const c1 = increment();
+const c2 = increment();
+const c3 = increment();
+
+console.log(c1, c2, c3);
+console.log(typeof(increment));
+console.log(typeof(c1));
