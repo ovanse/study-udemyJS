35696c55ae60bad55df5fdec304ec859938e2370
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-11-28 14:21:09 +1000

    recursion

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
+function pow(x, n) {
+  if (n === 1) {
+    return x;
+  } else {
+    return x * pow(x, n - 1);
+  }
+}
 
+console.log(pow(2, 1));
+console.log(pow(2, 2));
+console.log(pow(2, 3));
+console.log(pow(2, 4));
+console.log(pow(2, 5));
