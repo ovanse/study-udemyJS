ac886952d4310e7ac6d57775f8d89f9c1e6f50a1
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-11-30 12:43:52 +1000

    classList1

--- js/script.js
+++ js/script.js
@@ -1,21 +1,15 @@
 'use strict';
 
-const bigint = 111111111111111111566666666666666666666666666666666n;
+const btns = document.querySelectorAll('button');
 
-const sameBigint = BigInt(111111111111111111566666666666666666666666666666666);
+console.log(btns[0].classList.length);
+console.log(btns[0].classList.item(1));
+console.log(btns[0].classList.remove('blue'));
+console.log(btns[0].classList.toggle('blue'));
 
-console.log(typeof bigint);
-console.log(sameBigint);
-
-// console.log(5n + 1); // error
-// console.log(Math.round(5n)); // error
-console.log(1n + 2n);
-console.log(5n / 2n);
-console.log(2n > 5);
-console.log(2n == 2);
-console.log(2n === 2);
-
-let otherBigint = 1n;
-let number = 2;
-console.log(otherBigint + BigInt(number));
-console.log(Number(otherBigint) + number);
+console.log(btns[1].classList.add('red', 'otherClass'));
+if (btns[1].classList.contains('red')) {
+  console.log('red');
+} else {
+  console.log('red absence');
+}
