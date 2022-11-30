b198827caefa9cc92389ff2463236b6d75874100
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-11-30 13:21:59 +1000

    delegate event

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 'use strict';
 
-const btns = document.querySelectorAll('button');
+const btns = document.querySelectorAll('button'),
+  wrapper = document.querySelector('.btn-block');
 
-btns[0].addEventListener('click', () => {
-  if (!btns[1].classList.contains('red')) {
-    btns[1].classList.add('red');
-  } else {
-    btns[1].classList.remove('red');
+wrapper.addEventListener('click', (event) => {
+  if (
+    event.target &&
+    event.target.tagName == 'BUTTON' &&
+    event.target.classList.contains('blue')
+  ) {
+    btns[1].classList.toggle('red');
   }
 });
 
-console.log(btns[0].className);
+const btn = document.createElement('button');
+btn.classList.add('blue');
+wrapper.append(btn);
