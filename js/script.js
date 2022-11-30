e1875e4c8ff37eab31c99d6a4e701d62b7b00468
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-11-30 13:54:57 +1000

    interval

--- js/script.js
+++ js/script.js
@@ -1,18 +1,9 @@
 'use strict';
 
-const btns = document.querySelectorAll('button'),
-  wrapper = document.querySelector('.btn-block');
-
-wrapper.addEventListener('click', (event) => {
-  if (
-    event.target &&
-    event.target.matches('button.blue') &&
-    event.target.classList.contains('blue')
-  ) {
-    btns[1].classList.toggle('red');
-  }
-});
-
-const btn = document.createElement('button');
-btn.classList.add('blue');
-wrapper.append(btn);
+const timerId = setTimeout(
+  function (text) {
+    console.log(text);
+  },
+  2000,
+  'Hello'
+);
