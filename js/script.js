3ef0af139d610cdf2e7ba5c7f9d3f4899f8f8da2
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-11-30 11:46:53 +1000

    map3

--- js/script.js
+++ js/script.js
@@ -12,9 +12,12 @@
 console.log(user); */
 
 const shops = [{ rice: 500 }, { oil: 200 }, { bread: 50 }];
+const budget = [5000, 15000, 25000];
 
 const map = new Map();
 
-map.set(shops[0], 5000).set(shops[1], 15000).set(shops[2], 25000);
+shops.forEach((shop, i) => {
+  map.set(shop, budget[i]);
+});
 
 console.log(map);
