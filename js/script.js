bc99f493a2cc7c80e6993d0f24d8a2935e5b50a2
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-11-30 12:19:24 +1000

    set2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,35 @@
 'use strict';
 
-const arr = [1, 1, 2, 2, 4, 5, 6, 5];
+const arr = ['Alex', 'Anna', 'Oleg', 'Alex'];
 const set = new Set(arr);
 
+function unique(arr) {
+  return Array.from(new Set(arr));
+}
+console.log(unique(arr));
+
+set.add('Ivan');
+set.add('Oleg');
+console.log(set);
+
+if (set.has('Anna')) {
+  set.delete('Anna');
+}
+console.log(set);
+
+console.log(set.size);
+
+for (let value of set) {
+  console.log(value);
+}
+
+set.forEach((value, valueAgain, set) => {
+  console.log(value, valueAgain);
+});
+
+console.log(set.values());
+console.log(set.keys());
+console.log(set.entries());
+
+set.clear();
 console.log(set);
