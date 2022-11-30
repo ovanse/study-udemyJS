add8f4e448324479c46c80f73f7afff9265c26b8
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-11-30 14:23:53 +1000

    лучше рекурсивно setTimeout а не setInterval

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 'use strict';
 
-const timerId = setTimeout(logger, 2000);
-clearInterval(timerId);
+const btn = document.querySelector('.btn');
+let timerId,
+  i = 0;
+
+btn.addEventListener('click', () => {
+  // const timerId = setTimeout(logger, 2000);
+  timerId = setInterval(logger, 2000);
+});
 
 function logger() {
+  if (i === 3) {
+    clearInterval(timerId);
+  }
   console.log('text');
+  i++;
 }
+
+let id = setTimeout(function log() {
+  console.log('Hello');
+  id = setTimeout(log, 500);
+}, 500);
