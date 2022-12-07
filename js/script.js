bd18a6d1835eb5d2ec20941e2d17388c4ff38e51
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-12-07 17:03:06 +1000

    size and offset of windwo and elements

--- js/script.js
+++ js/script.js
@@ -1,11 +1,33 @@
 'use strict';
 
-let start = new Date();
+const box = document.querySelector('.box');
+const btn = document.querySelector('button');
 
-for (let i = 0; i < 100000; i++) {
-  let some = i ** 3;
-}
+const clientWidth = box.clientWidth;
+const clientHeight = box.clientHeight;
+console.log(`clientWidth: ${clientWidth}, clientHeight: ${clientHeight}`);
 
-let end = new Date();
+const offsetWidth = box.offsetWidth;
+const offsetHeight = box.offsetHeight;
+console.log(`offsetWidth: ${offsetWidth}, offsetHeight: ${offsetHeight}`);
 
-console.log(`Cycle work time ${end - start} milliseconds`);
+const scrollWidth = box.scrollWidth;
+const scrollHeight = box.scrollHeight;
+console.log(`scrollWidth: ${scrollWidth}, scrollHeight: ${scrollHeight}`);
+
+btn.addEventListener(
+  'click',
+  () => {
+    box.style.height = box.scrollHeight + 'px';
+    console.log(box.scrollTop);
+  },
+  false
+);
+
+console.log(box.getBoundingClientRect().top);
+
+const style = window.getComputedStyle(box);
+console.log(style.display);
+
+console.log(document.documentElement.clientWidth);
+console.log(document.documentElement.scrollTo);
