be7f242d201a8c341c0defb89a0cb5945845e6ba
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-12-08 13:30:37 +1000

    Constructor ES5

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
 'use strict';
 
-const box = document.querySelector('.box');
+function User(name, id) {
+  this.name = name;
+  this.id = id;
+  this.human = true;
+  this.hello = function () {
+    console.log(`Hello ${this.name}`);
+  };
+}
 
-let observer = new MutationObserver((mutationRecords) => {
-  console.log(mutationRecords);
-});
+User.prototype.exit = function () {
+  console.log(`User ${this.name} exited.`);
+};
 
-observer.observe(box, {
-  childList: true,
-});
+const ivan = new User('Ivan', 0);
+const alex = new User('Alexey', 1);
 
-// observer.disconnect();
+ivan.exit();
+alex.exit();
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
