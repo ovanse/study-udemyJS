30cccdd2372e28049cacca206f85c1f5bc9308f1
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-12-08 14:02:24 +1000

    context 2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 'use strict';
 
-function showThis() {
+function showThis(a, b) {
   console.log(this);
+  function sum() {
+    console.log(this);
+    return a + b;
+  }
+
+  console.log(sum());
 }
 
-showThis();
+showThis(4, 5);
+
+const obj = {
+  a: 20,
+  b: 15,
+  sum: function () {
+    console.log(this);
+  },
+};
+obj.sum();
 
 // 1) Обычная функция - this = window (или undefined если используется use strict)
+// 2) Контекст у методов объекта - сам объект.
