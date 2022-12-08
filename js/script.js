cc0e49de4c527bdd259efde13949feea90f3b2f3
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-12-08 16:57:30 +1000

    ES6 classes

--- js/script.js
+++ js/script.js
@@ -1,20 +1,33 @@
 'use strict';
 
-const btn = document.querySelector('button');
-btn.addEventListener('click', function () {
-  console.log(this);
-  this.style.backgroundColor = 'red';
-});
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
 
-const obj = {
-  nym: 5,
-  sayNumber: function () {
-    const say = () => {
-      console.log(this);
-    };
+  calcArea() {
+    return this.height * this.width;
+  }
+}
 
-    say();
-  },
-};
+class ColoredRectangleWithText extends Rectangle {
+  constructor(height, width, text, bgColor) {
+    super(height, width);
+    this.text = text;
+    this.bgColor = bgColor;
+  }
 
-obj.sayNumber();
+  showMyProps() {
+    console.log(`Text: '${this.text}'; color: ${this.bgColor}`);
+  }
+}
+
+const square = new Rectangle(10, 10);
+const long = new Rectangle(20, 100);
+const div = new ColoredRectangleWithText(25, 10, 'Hello World', 'red');
+
+console.log(square.calcArea());
+console.log(long.calcArea());
+console.log(div.calcArea());
+div.showMyProps();
