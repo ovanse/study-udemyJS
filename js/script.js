7e03269ba8ba3e562d9fb47c9e1e6d0aa5e32623
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-12-08 13:32:56 +1000

    Constructor ES6

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 'use strict';
 
-function User(name, id) {
-  this.name = name;
-  this.id = id;
-  this.human = true;
-  this.hello = function () {
-    console.log(`Hello ${this.name}`);
-  };
+class User {
+  constructor(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+  }
+  hello() {
+    console.log(`Hello ${this.name}.`);
+  }
+  exit() {
+    console.log(`User ${this.name} leaved.`);
+  }
 }
 
 User.prototype.exit = function () {
