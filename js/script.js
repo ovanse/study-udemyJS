6d8266892054e34c97a102785aa17d7317ec8510
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-12-08 15:56:36 +1000

    context 4

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
 'use strict';
 
-function User(name, id) {
-  this.name = name;
-  this.id = id;
-  this.human = true;
-  this.hello = function () {
-    console.log(`Hello ${this.name}!`);
-  };
+function sayName(surname) {
+  console.log(this);
+  console.log(this.name + surname);
 }
-let ivan = new User('Ivan', 0);
+
+const user = {
+  name: 'John',
+};
+
+sayName.call(user, 'Smith');
+sayName.apply(user, ['Smith']);
+
+function count(num) {
+  return this * num;
+}
+
+const double = count.bind(2);
+console.log(double(3));
+console.log(double(13));
 
 // 1) Обычная функция - this = window (или undefined если используется use strict).
 // 2) Контекст у методов объекта - сам объект.
 // 3) this в конструктарах и классах - это новый экземпляр объекта.
+// 4) Ручная привязка this: call, apply, bind
