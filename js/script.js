8f68f2800ca2f585e1bd95cbb09957804bdd77ab
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-12-13 13:25:53 +1000

    AJAX with onReadystaychange

--- js/script.js
+++ js/script.js
@@ -1,91 +1,22 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const movieDB = {
-    movies: [
-      'Логан',
-      'Лига справедливости',
-      'Ла-ла лэнд',
-      'Одержимость',
-      'Скотт Пилигрим против...',
-    ],
-  };
+const inputRub = document.querySelector('#rub'),
+  inputUsd = document.querySelector('#usd');
 
-  const adv = document.querySelectorAll('.promo__adv img'),
-    poster = document.querySelector('.promo__bg'),
-    genre = poster.querySelector('.promo__genre'),
-    movieList = document.querySelector('.promo__interactive-list'),
-    addForm = document.querySelector('form.add'),
-    addInput = addForm.querySelector('.adding__input'),
-    checkbox = addForm.querySelector('[type="checkbox"]');
+inputRub.addEventListener('input', () => {
+  const request = new XMLHttpRequest();
 
-  // Funtions ===
+  request.open('GET', './js/current.json');
+  request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  request.send();
 
-  const createMovieList = (films, parent) => {
-    parent.innerHTML = '';
-    sortArr(films);
-    films.forEach((film, i) => {
-      parent.innerHTML += `
-      <li class="promo__interactive-item">${i + 1} ${film}
-      <div class="delete"></div>
-      </li>`;
-    });
-
-    document.querySelectorAll('.delete').forEach((btn, i) => {
-      btn.addEventListener('click', () => {
-        btn.parentElement.remove();
-        movieDB.movies.splice(i, 1);
-        createMovieList(films, parent);
-      });
-    });
-  };
-
-  const deleteAdv = (arr) => {
-    arr.forEach((item) => {
-      item.remove();
-    });
-  };
-
-  const makeChanges = () => {
-    genre.textContent = 'драма';
-    poster.style.backgroundImage = "url('../img/bg.jpg')";
-  };
-
-  const sortArr = (arr) => {
-    arr.sort();
-  };
-
-  // ======================
-
-  // Event Listeners ======
-
-  addForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    let newFilmTitle = addInput.value;
-    const isFavorite = checkbox.checked;
-
-    if (newFilmTitle) {
-      if (newFilmTitle.length > 21) {
-        newFilmTitle = `${newFilmTitle.substring(0, 22)}...`;
-      }
-
-      if (isFavorite) {
-        console.log('Add favorite film');
-      }
-
-      movieDB.movies.push(newFilmTitle);
-      createMovieList(movieDB.movies, movieList);
+  request.addEventListener('readystatechange', () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.response);
+      const data = JSON.parse(request.response);
+      inputUsd.value = (+inputRub.value / +data.current.usd).toFixed(2);
+    } else {
+      inputUsd.value = "can't recieve exchange rate";
     }
-
-    event.target.reset();
   });
-
-  // ===================
-
-  deleteAdv(adv);
-
-  makeChanges();
-
-  createMovieList(movieDB.movies, movieList);
 });
