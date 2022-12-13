d725154782fdaa9dfea7809223fe75e6cb2c8e5d
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2022-12-13 17:09:42 +1000

    Fretch API

--- js/script.js
+++ js/script.js
@@ -1,33 +1,13 @@
 'use strict';
 
-console.log('Запрос данных...');
-
-const req = new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    console.log('Подготовка данных...');
-
-    const product = {
-      name: 'TV',
-      price: '2000',
-    };
-
-    resolve(product);
-  }, 2000);
-});
-
-req
-  .then((product) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        product.status = 'ordered';
-        resolve(product);
-      }, 2000);
-    });
-  })
-  .then((data) => {
-    data.modify = true;
-    return data;
-  })
-  .then((data) => {
-    console.log(data);
-  });
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    name: 'Alex',
+  }),
+  headers: {
+    'Content-type': 'application/json',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
