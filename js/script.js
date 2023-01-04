566e1abebc981d43f33e9cb18d419c911660c4fc
Andrei Ovchinnikov <ansdrei.s.ovchinnikov@gmail.com>
2023-01-04 16:15:00 +1000

    working calculator calories

--- js/script.js
+++ js/script.js
@@ -425,4 +425,80 @@ document.addEventListener('DOMContentLoaded', () => {
             dots[slideIndex - 1].style.opacity = 1;
         });
     });
+
+    // Calculator
+
+    const result = document.querySelector('.calculating__result span');
+    let sex = 'female',
+        height,
+        weight,
+        age,
+        ratio = 1.375;
+
+    function calcTotal() {
+        if (!sex || !height || !weight || !age || !ratio) {
+            result.textContent = '____';
+            return;
+        }
+
+        if (sex === 'female') {
+            result.textContent = (
+                (447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) *
+                ratio
+            ).toFixed();
+        } else {
+            result.textContent = (
+                (88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) *
+                ratio
+            ).toFixed();
+        }
+    }
+
+    calcTotal();
+
+    function getStaticInformation(parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+
+        elements.forEach((elem) => {
+            elem.addEventListener('click', (event) => {
+                if (event.target.getAttribute('data-ratio')) {
+                    ratio = +event.target.getAttribute('data-ratio');
+                } else {
+                    sex = event.target.getAttribute('id');
+                }
+
+                elements.forEach((elem) => {
+                    elem.classList.remove(activeClass);
+                });
+                event.target.classList.add(activeClass);
+
+                calcTotal();
+            });
+        });
+    }
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('#activity', 'calculating__choose-item_active');
+
+    function getDynamicInformation(selector) {
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () => {
+            switch (input.getAttribute('id')) {
+                case 'height':
+                    height = +input.value;
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;
+            }
+            calcTotal();
+        });
+    }
+
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
 });
