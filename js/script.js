64c16f472d4b110f5ddd8106d9dd726bad4752c3
Andrei Ovchinnikov <ansdrei.s.ovchinnikov@gmail.com>
2023-01-04 15:10:58 +1000

    RegExp: test

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 'use strict';
 
-// const ans = prompt('Введите ваше имя');
+const ans = prompt('Введите ваше имя');
 
 //i независимо от регистра
 //g глобальный поиск (не только первое совпадение паттерна)
 //m многострочный режим
 // const reg = /n/i; // флаг глобальности не работает на простом search
 const reg = /n/gi;
+console.log(reg.test(ans));
 
 // console.log(ans.search(reg));
 // console.log(ans.match(reg));
 
-const pass = prompt('Password');
-console.log(pass);
-console.log(pass.replace(/\./g, '*'));
+// const pass = prompt('Password');
+// console.log(pass);
+// console.log(pass.replace(/\./g, '*'));
 
-console.log('12-34-56'.replace(/-/g, ':'));
+// console.log('12-34-56'.replace(/-/g, ':'));
