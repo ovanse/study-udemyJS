b02caf0c356b2a79cc75b67515f2dbe89d8eb50a
Andrei Ovchinnikov <ansdrei.s.ovchinnikov@gmail.com>
2023-01-07 15:07:12 +1000

    геттеры и сеттеры

--- js/script.js
+++ js/script.js
@@ -1,22 +1,16 @@
 'use strict';
 
-// const ans = prompt('Введите ваше имя');
+const persone = {
+    name: 'Alex',
+    age: 25,
 
-//i независимо от регистра
-//g глобальный поиск (не только первое совпадение паттерна)
-//m многострочный режим
-// const reg = /n/i; // флаг глобальности не работает на простом search
-// const reg = /n/gi;
-// console.log(reg.test(ans));
+    get userAge() {
+        return this.age;
+    },
+    set userAge(num) {
+        this.age = num;
+    },
+};
 
-// console.log(ans.search(reg));
-// console.log(ans.match(reg));
-
-// const pass = prompt('Password');
-// console.log(pass);
-// console.log(pass.replace(/\./g, '*'));
-
-// console.log('12-34-56'.replace(/-/g, ':'));
-
-const str = 'My name is R2D2';
-console.log(str.match(/\w\d\w\d/i));
+persone.userAge = 30;
+console.log(persone.userAge);
