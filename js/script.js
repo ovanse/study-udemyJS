7287c2aa5ba773426456a7a58e80777f4ae0fc71
Andrei Ovchinnikov <ansdrei.s.ovchinnikov@gmail.com>
2023-01-08 20:31:39 +1000

    modules1

--- js/script.js
+++ js/script.js
@@ -1,65 +1,25 @@
 'use strict';
 
-class User {
-    constructor(name, age) {
-        this._name = name;
-        this._age = age;
-    }
+const app = '123';
 
-    say = () => {
-        console.log(`Имя пользователя: ${this._name}, возраст ${this._age}`);
+const number = 1;
+
+(function () {
+    let number = 2;
+    console.log(number);
+    console.log(number + 3);
+})();
+
+console.log(number);
+
+const user = (function () {
+    const privat = function () {
+        console.log(`I am private!`);
     };
 
-    get name() {
-        return this._name;
-    }
-    set name(name) {
-        if (typeof name === 'string' && name !== '') {
-            this._name = name;
-            return true;
-        } else {
-            console.log(`Недопустимое значение!`);
-            return false;
-        }
-    }
+    return {
+        sayHello: privat,
+    };
+})();
 
-    get age() {
-        return this._age;
-    }
-    set age(age) {
-        if (typeof age === 'number' && age > 0 && age < 110) {
-            this._age = age;
-            return true;
-        } else {
-            console.log(`Недопустимое значение!`);
-            return false;
-        }
-    }
-
-    #lastName = 'Ovchinnikov';
-
-    get lastName() {
-        return this.#lastName;
-    }
-    set lastName(str) {
-        if (typeof str === 'string' && str !== '') {
-            this.#lastName = str;
-            return true;
-        } else {
-            console.log(`Недопустимое значение!`);
-            return false;
-        }
-    }
-}
-
-const ivan = new User('Ivan', 27);
-console.log(ivan.name);
-console.log(ivan.age);
-
-ivan.age = 30;
-ivan.age = 300;
-console.log(ivan.age);
-
-console.log(ivan.lastName);
-
-ivan.say();
+user.sayHello();
