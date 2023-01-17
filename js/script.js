71db57722d725e3e42f8c27c27cdfb76d63109a7
Andrei Ovchinnikov <andrei.s.ovchinnikov@gmail.com>
2023-01-17 14:26:53 +1000

    собственные ошибки

--- js/script.js
+++ js/script.js
@@ -1,15 +1,38 @@
 'use strict';
 
+const data = [
+    {
+        id: 'box',
+        tag: 'div',
+    },
+    {
+        id: 'fff',
+        tag: 'nav',
+    },
+    {
+        id: 'circle',
+        tag: '',
+    },
+];
+
 try {
-    console.log('Normal');
-    console.log(a);
-    console.log('Result');
-} catch (error) {
-    console.log(error.name);
-    console.log(error.message);
-    console.log(error.stack);
-} finally {
-    console.log('Finally');
+    data.forEach((blockObj, i) => {
+        const block = document.createElement(blockObj.tag);
+
+        if (blockObj.id === '') throw new Error(`В данных под номером ${i + 1} нет id`);
+
+        block.setAttribute('id', blockObj.id);
+        document.body.append(block);
+    });
+} catch (e) {
+    if (e.name === 'Error') {
+        console.error(e.name);
+        console.log(e.message);
+        console.log(e.stack);
+    } else throw e;
 }
 
-console.log('Still normal');
+const err = new SyntaxError('testtesttest');
+console.log(err.name);
+console.log(err.message);
+console.log(err.stack);
